176ce8b17218e55aad44dfb94652d46ae0cdede0
Anya Sedova <anutkased05@gmail.com>
2020-07-17 11:23:44 +0000

    handling clicks

---
+++ portfolio/src/main/webapp/piano.js
@@ -0,0 +1,22 @@
+/*
+* Plays audio by key number
+*/
+
+function playNote(e) {
+  var audio = document.querySelector(`audio[data-key = "${e}"]`);
+  audio.currentTime = 0;  
+  audio.play();
+}
+
+/*
+* Plays after clicking
+*/
+
+$(document).ready(function(){
+  $(".key").click(function(){
+    key = $(this).attr('data-key');
+    playNote(key);
+  });
+});
+
+
